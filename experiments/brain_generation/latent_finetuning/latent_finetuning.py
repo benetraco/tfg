3377ddc0df1201dd28e2907c9d5759c43324b34b
benetraco <benet.ramio@estudiantat.upc.edu>
2025-03-14 20:42:08 +0100

    some small modifications for new experiments in latent_finetuning

--- experiments/brain_generation/latent_finetuning/latent_finetuning.py
+++ experiments/brain_generation/latent_finetuning/latent_finetuning.py
@@ -151,6 +151,10 @@ class LatentFineTuning:
             noise_scheduler.set_timesteps(self.config['training']['noise_scheduler']['num_inference_timesteps'])
         else:
             raise ValueError("Noise scheduler type not recognized. Please choose between 'DDPM' and 'DDIM'.")
+        
+        # Consider using the noise scheduler from the pretrained model TO BE TRIED
+        # if self.config['training']['noise_scheduler']['use_pretrained']:
+        #     noise_scheduler = DDPMScheduler.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="scheduler")
         return optimizer, lr_scheduler, noise_scheduler, num_update_steps_per_epoch
 
 
@@ -287,21 +291,21 @@ class LatentFineTuning:
                     
 
                     # **Randomly use empty prompt embeddings for a portion of the batch**
-                    # use_uncond = torch.rand(bs) < 0.1  # 10% of the batch uses unconditional embeddings
-                    # text_embeddings_batch = []
-                    # for i in range(bs):
-                    #     if use_uncond[i]: 
-                    #         text_embeddings_batch.append(self._get_embeddings("", self.ldm))  # Empty prompt
-                    #     else:
-                    #         text_embeddings_batch.append(self.text_embeddings)  # Normal prompt
+                    use_uncond = torch.rand(bs) < 0.1  # 10% of the batch uses unconditional embeddings
+                    text_embeddings_batch = []
+                    for i in range(bs):
+                        if use_uncond[i]: 
+                            text_embeddings_batch.append(self._get_embeddings("", self.ldm))  # Empty prompt
+                        else:
+                            text_embeddings_batch.append(self.text_embeddings)  # Normal prompt
                     
-                    # text_embeddings_batch = [t.to(self.accelerator.device) for t in text_embeddings_batch]
-                    # text_embeddings_batch = torch.stack(text_embeddings_batch).squeeze(1)
+                    text_embeddings_batch = [t.to(self.accelerator.device) for t in text_embeddings_batch]
+                    text_embeddings_batch = torch.stack(text_embeddings_batch).squeeze(1)
 
                     # Forward pass
                     noisy_images = self.noise_scheduler.add_noise(latents, noise, timesteps)
-                    # noise_pred = self.model(noisy_images, timesteps, encoder_hidden_states=text_embeddings_batch.expand(bs, -1, -1)).sample
-                    noise_pred = self.model(noisy_images, timesteps, encoder_hidden_states=self.text_embeddings.expand(bs, -1, -1)).sample
+                    noise_pred = self.model(noisy_images, timesteps, encoder_hidden_states=text_embeddings_batch.expand(bs, -1, -1)).sample
+                    # noise_pred = self.model(noisy_images, timesteps, encoder_hidden_states=self.text_embeddings.expand(bs, -1, -1)).sample
                     loss = F.mse_loss(noise_pred.float(), noise.float(), reduction='mean')
                     
                     # Logging
