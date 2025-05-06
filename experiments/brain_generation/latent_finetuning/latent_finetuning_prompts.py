b381379000627c38161f8f4de745a582509a52db
benetraco <benet.ramio@estudiantat.upc.edu>
2025-05-06 15:41:24 +0200

    altres petites modificacions latent

--- experiments/brain_generation/latent_finetuning/latent_finetuning_prompts.py
+++ experiments/brain_generation/latent_finetuning/latent_finetuning_prompts.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os, sys
 # Restrict PyTorch to use only GPU X
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 import gc
 import yaml
 import math
