c7253bfdb266a3649c2460fc750c77017ae488f8
benetraco <benet.ramio@estudiantat.upc.edu>
2025-05-28 13:49:36 +0200

    modifications to obtain the GT of the generated lesions

--- experiments/hole_pipeline.py
+++ experiments/hole_pipeline.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 from diffusers import StableDiffusionInpaintPipeline
 import torch
 from typing import Optional, Tuple
-
+from torchvision.transforms.functional import to_tensor
+from skimage import measure
 
 class CreateSample:
     def __init__(self, scanner: str, lesion_bbox: Optional[Tuple[int, int, int, int]], real_brain: bool, seed: int = 17844):
@@ -26,9 +27,85 @@ class CreateSample:
         self.real_scanner_path = f"/home/benet/data/biomarkem2D/test/{scanner}"
         self.seed = seed
         self.wm_gm_mask = None
-        self.lesion_mask = None
+        self.lesion_bbox_mask = None
         self.healthy_brain = None
-        self.image = self.generate_image()
+        self.lesioned_brain = self.generate_image()
+        self.lesion_mask, self.diff_bbox = self.calculate_difference()
+        self.segmented_lesion = self.segment_lesion()
+
+    def segment_lesion(self):
+        """"""
+        # in the image with lesion, paint a red line in the lesion area from the difference image
+        segmented_lesion = np.array(self.lesioned_brain)
+        lesion_mask = np.array(self.lesion_mask)
+        contours = measure.find_contours(lesion_mask > 0, 0.5)
+        for contour in contours:
+            for y, x in contour.astype(np.int32):
+                if 0 <= x < segmented_lesion.shape[1] and 0 <= y < segmented_lesion.shape[0]:
+                    # paint the pixel red
+                    segmented_lesion[y, x] = [255, 0, 0]
+        # convert to PIL Image
+        segmented_lesion = Image.fromarray(segmented_lesion)
+
+
+        return segmented_lesion
+
+
+    def calculate_difference(self):
+        """
+        Calculates the difference between the original healthy brain image and the generated image with the lesion.
+        """
+        if self.healthy_brain.size != self.lesioned_brain.size:
+            # resize the generated image to match the healthy brain image size
+            self.lesioned_brain = self.lesioned_brain.resize(self.healthy_brain.size, Image.LANCZOS)
+
+        healty_tensor = to_tensor(self.healthy_brain)
+        lesioned_tensor = to_tensor(self.lesioned_brain)
+
+        # diff_tensor is the difference to white between the lesioned and healthy brain images
+        diff_tensor = lesioned_tensor - healty_tensor
+        diff_image = diff_tensor.mean(dim=0)
+
+        diff_bbox = diff_image.numpy()
+        # restrict to only the values inside the bounding box of the lesion
+        assert self.lesion_bbox is not None, "Lesion bounding box must be defined to calculate the difference."
+        x1, y1, x2, y2 = self.lesion_bbox
+        diff_bbox = diff_bbox[y1:y2, x1:x2]
+
+        # normalize the difference image to the range [0, 1]
+        diff_bbox = (diff_bbox - diff_bbox.min()) / (diff_bbox.max() - diff_bbox.min())
+        #plot an histogram of the difference image
+        plt.hist(diff_bbox.ravel(), bins=256, color='blue', alpha=0.7)
+        plt.title("Histogram of the Difference Image")
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+        plt.show()
+        # binarize the difference image to create a mask
+        # diff_bbox = (diff_bbox > 0.7).astype(np.uint8) * 255
+        
+        # Convert the normalized diff_bbox to 8-bit grayscale
+        diff_bbox_8bit = (diff_bbox * 255).astype(np.uint8)
+        # Apply Otsu's thresholding
+        otsu_thresh, diff_bbox_bin = cv2.threshold(diff_bbox_8bit, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        print(f"Otsu's threshold: {otsu_thresh}")
+        diff_bbox = diff_bbox_bin
+
+        # apply a opening operation to remove small noise
+        diff_bbox = cv2.morphologyEx(diff_bbox, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
+        # apply a closing operation to fill small holes
+        # diff_bbox = cv2.morphologyEx(diff_bbox, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+        # create a image of the size of the lesion_bbox_mask with the difference image
+        lesion_mask = np.zeros_like(np.array(self.lesion_bbox_mask))
+
+        lesion_mask[y1:y2, x1:x2] = diff_bbox
+        # convert to PIL Image
+        diff_bbox_image = Image.fromarray(diff_bbox, mode='L')
+        lesion_mask_image = Image.fromarray(lesion_mask, mode='L')
+
+        return lesion_mask_image, diff_bbox_image
+        # return diff_bbox, diff_bbox_image
 
     def generate_image(self):
         """
@@ -78,11 +155,11 @@ class CreateSample:
         
         # Check if lesion bbox is within the WM/GM mask
         x1, y1, x2, y2 = self.lesion_bbox
-        lesion_mask = self.wm_gm_mask[y1:y2, x1:x2]
+        lesion_bbox_mask = self.wm_gm_mask[y1:y2, x1:x2]
 
         # if a 90% of the lesion mask is within the WM/GM mask, return True
-        lesion_mask = lesion_mask > 0
-        return np.sum(lesion_mask) / lesion_mask.size >= 0.9
+        lesion_bbox_mask = lesion_bbox_mask > 0
+        return np.sum(lesion_bbox_mask) / lesion_bbox_mask.size >= 0.9
     
     def get_wm_gm_mask(self):
         img_arr = np.array(self.healthy_brain).astype(np.float32)
@@ -140,7 +217,7 @@ class CreateSample:
     
     def get_lesion_image(self):
         
-        if self.lesion_mask is None:
+        if self.lesion_bbox_mask is None:
             self.generate_mask(self.healthy_brain.size)
         model = self.load_inpainting_pipeline()
 
@@ -148,14 +225,17 @@ class CreateSample:
         if self.healthy_brain.mode == 'L':
             self.healthy_brain = self.healthy_brain.convert('RGB')
 
-        return model(
+        lesioned_image = model(
             prompt="SHIFTS multiple sclerosis lesion in a FLAIR MRI",
             image=self.healthy_brain,
-            mask_image=self.lesion_mask,
+            mask_image=self.lesion_bbox_mask,
             num_inference_steps=25,
             guidance_scale=5,
+            generator=torch.Generator(device="cuda").manual_seed(self.seed)
         ).images[0]
 
+        return lesioned_image
+    
     def generate_mask(self, size:tuple):
         
         mask = np.zeros(size, dtype=np.uint8)
@@ -165,30 +245,36 @@ class CreateSample:
         # Convert to PIL Image
         mask_image = Image.fromarray(mask, mode='L')
 
-        self.lesion_mask = mask_image
+        self.lesion_bbox_mask = mask_image
     
-    def load_inpainting_pipeline(self, model_id:str="benetraco/ms-lesion-inpainting-vh-shifts", device:str="cuda"):
+    def load_inpainting_pipeline(self, model_id:str="benetraco/ms-lesion-inpainting-vh-shifts-wmh2017_v2", device:str="cuda"):
         pipe = StableDiffusionInpaintPipeline.from_pretrained(model_id, torch_dtype=torch.float32, safety_checker=None)
         # pipe.set_progress_bar_config(disable=True)
         return pipe.to(device)
     
     def plot_image(self):
         """
-        Displays the original image, the mask, and the generated image with the lesion.
+        Displays the original image, the mask, the generated image with the lesion, the difference image, and the segmented lesion.
         """
-        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        fig, axes = plt.subplots(1, 5, figsize=(20, 4))
         axes[0].imshow(self.healthy_brain, cmap='gray')
-        axes[0].set_title("Original Image")
+        axes[0].set_title("Healthy Brain")
         axes[0].axis('off')
 
-        if self.lesion_mask is not None:
-            axes[1].imshow(self.lesion_mask, cmap='gray')
-            axes[1].set_title("Lesion Mask")
-            axes[1].axis('off')
+        axes[1].imshow(self.lesion_bbox_mask, cmap='gray')
+        axes[1].set_title("Lesion BBox Mask")
+        axes[1].axis('off')
 
-        axes[2].imshow(self.image, cmap='gray')
-        axes[2].set_title("Image with Lesion")
+        axes[2].set_title("Lesioned Brain")
         axes[2].axis('off')
+
+        axes[3].imshow(self.lesion_mask, cmap='gray')
+        axes[3].set_title("Lesion Mask")
+        axes[3].axis('off')
+
+        axes[4].imshow(self.segmented_lesion)
+        axes[4].set_title("Segmented Lesion")
+        axes[4].axis('off')
 
         plt.tight_layout()
         plt.show()
